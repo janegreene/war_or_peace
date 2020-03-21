290b8036144e0f9c381449706ba32b38f3ad34ec
Jane Greene <janegreenecasa@gmail.com>
2020-03-21 15:22:12 -0600

    updates to player

--- lib/player.rb
+++ lib/player.rb
@@ -3,10 +3,14 @@ require 'pry'
 class Player
   attr_reader :name,
               :deck
-  
+
   def initialize(name, deck)
     @name = name
     @deck = deck
 
+  end
+
+  def has_lost?
+    @deck.cards.empty? #deck == []
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -20,13 +20,22 @@ class PlayerTest < Minitest::Test
   end
 
   def test_has_lost?
-    skip
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     deck = Deck.new([card1, card2, card3])
 
     player = Player.new('Clarisa', deck)
+    assert_equal false, player.has_lost?
+
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+
+    player.deck.remove_card
+    assert_equal true, player.has_lost?
 
   end
 #binding.pry
