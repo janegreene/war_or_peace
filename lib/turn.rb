0cf9cfdc331f49402c79d85e93dbf02d13dfe98b
Jane Greene <janegreenecasa@gmail.com>
2020-03-21 20:52:09 -0600

    Add test for basic type

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,5 +11,21 @@ class Turn
     @spoils_of_war = []
   end
 
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    end
+    # if 0 index of player1 != 0 index of player2
+    # :basic
+    #elsif
+    # :war
+    #else
+    # :mutually_assured_destruction
+  end
+
+  def winner
+
+  end
+
 
 end
