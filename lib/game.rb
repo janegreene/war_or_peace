ee7df4b843cb6845a457811b4fa50b1f6bb96191
Jane Greene <janegreenecasa@gmail.com>
2020-03-22 20:29:44 -0600

    Add game class and updates to runner

---
+++ lib/game.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Game
+  def initialize
+
+  end
+
+  def start_game
+
+  end
+
+  def shuffle
+
+  end
+end
--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,12 +2,9 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game'
 require 'pry'
 
-p Welcome to War! (or Peace) This game will be played with 52 cards.
-p The players today are Megan and Aurora.
-p Type 'GO' to start the game!
-p ------------------------------------------------------------------
 
 card1 = Card.new(:heart, 'Ace', 14)
 card2 = Card.new(:heart, 'King', 13)
